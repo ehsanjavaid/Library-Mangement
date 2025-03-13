bc3d64ed89456bae1f051f91163a24f0e2442d8f
Ehsan Javaid <ehsanjavaid321@gmail.com>
2025-03-13 10:07:04 +0500

    membership doctype complete

--- library_management/library_management/doctype/member/member.py
+++ library_management/library_management/doctype/member/member.py
@@ -2,9 +2,24 @@
 # For license information, please see license.txt
 
 import frappe
+import random 
 from frappe.model.document import Document
 
 
 class Member(Document):
+	def generate_membership_id(self):
+			prefix_map = {
+				"Student": "STD",
+				"Regular": "REG",
+				"Premium": "PRE"
+			}
+			prefix = prefix_map.get(self.membership_type, "GEN")
+			random_number = random.randint(1000, 9999)
+			return f"{prefix}-{random_number}" 
+	
 	def before_save(self):
-		 self.full_name = f"{self.first_name} {self.last_name}"
+		self.full_name = f"{self.first_name} {self.last_name}"
+		if self.payment_status == "Paid" and not self.membership_id:
+			self.membership_id = self.generate_membership_id()
+		
+		
