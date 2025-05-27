32abe197b55d91f030f5663e1235613db7717128
Ehsan Javaid <ehsanjavaid321@gmail.com>
2025-05-27 13:35:01 +0500

    Third party config whatsapp

--- library_management/library_management/doctype/member/member.js
+++ library_management/library_management/doctype/member/member.js
@@ -1,9 +1,14 @@
 // Copyright (c) 2025, ahsan and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Member", {
-//     refresh(frm) {
-//         pass
-
-// 	},
-// });
+frappe.ui.form.on("Member", {
+    refresh(frm) {
+        frm.add_custom_button(__('Contact Us'), function () {
+            let admin_number = "923138619329";
+            let member_name = frm.doc.name || frappe.doc.member_name;
+            let message = `Hello, I am ${member_name}. I would like to inquire about the library membership.`;
+            let whatsapp_url = `https://wa.me/${admin_number}?text=${encodeURIComponent(message)}`;
+            window.open(whatsapp_url, '_blank');
+        });
+	},
+});
