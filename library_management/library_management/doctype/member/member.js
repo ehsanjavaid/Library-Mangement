684f0f87e905f72ebf590c0f59de6de102825c28
Ehsan Javaid <ehsanjavaid321@gmail.com>
2025-05-27 16:13:32 +0500

    whatsapp interegrate with member with new message doctype

--- library_management/library_management/doctype/member/member.js
+++ library_management/library_management/doctype/member/member.js
@@ -4,11 +4,44 @@
 frappe.ui.form.on("Member", {
     refresh(frm) {
         frm.add_custom_button(__('Contact Us'), function () {
-            let admin_number = "923138619329";
-            let member_name = frm.doc.name || frappe.doc.member_name;
-            let message = `Hello, I am ${member_name}. I would like to inquire about the library membership.`;
-            let whatsapp_url = `https://wa.me/${admin_number}?text=${encodeURIComponent(message)}`;
-            window.open(whatsapp_url, '_blank');
+            frappe.call({
+                method: "frappe.client.get_list",
+                args: {
+                    doctype: "Whatsapp Saved Template",
+                    fields: ["message_body"],
+                    limit_page_length: 100,
+                },
+                callback: function (response) {
+                    if (!response.message || response.message.length == 0) {
+                        frappe.msgprint(__('No saved templates found.'))
+                        return;
+                    }
+                    let admin_number = "923138619329";
+                    let member_name = frm.doc.name || frappe.doc.member_name;
+
+                    const options = response.message.map(row => row.message_body.replace("{{member_name}}", member_name));
+                    let d = new frappe.ui.Dialog({
+                        title: __('Choose message to send'),
+                        fields: [
+                            {
+                                label: 'Message Options',
+                                fieldname: 'message',
+                                fieldtype: 'Select',
+                                options: options,
+                                reqd: 1
+                            }
+                        ],
+                        primary_action_label: __('Send On Whatsapp'),
+                        primary_action(values) {
+                            let whatsapp_url = `https://wa.me/${admin_number}?text=${encodeURIComponent(values.message)}`;
+                            window.open(whatsapp_url, '_blank');
+                            d.hide();
+                        }
+                    });
+                    d.show();
+                }
+            });
         });
-	},
+    }
 });
+
